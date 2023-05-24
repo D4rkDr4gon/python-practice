3c13b3890e94374d93a25783235c1ba5d1f09c35
Lucciano Campassi <106495109+lucciano19@users.noreply.github.com>
2023-05-24 19:15:48 -0300

    Add files via upload

---
+++ typehints.py
@@ -0,0 +1,12 @@
+#type hints
+
+#dsp de el nombre de la variable al poner : tipo de var lo especifico
+
+string: str = "hola mundo"
+print(type(string))
+
+integrer: int = 5
+print(type(integrer))
+
+flotante: float = 3.2
+print(type(flotante))
